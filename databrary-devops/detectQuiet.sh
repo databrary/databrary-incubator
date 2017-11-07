0e70fdb8246cfa7d0783da25fa76b397c80e6c58
Kanishka <nish2575@gmail.com>
2017-11-07 10:49:45 -0500

    Update detectQuiet.sh
    Add relative paths to match specific environment

--- databrary-devops/detectQuiet.sh
+++ databrary-devops/detectQuiet.sh
@@ -1,9 +1,14 @@
 #!/bin/bash                                                                     
 # expects an argument providing the email address to alert
+# ensure running as databrary (or demo, respectively) to connect to proper db
+# set these appropriately:
+src_path="/home/build/databrary"
+conf_dir="/home/databrary"
 
+cd $conf_dir
 while true
 do
-   res=`echo "select count(*) from upload" | ./runsql - | head -3 | grep '\s*0\s*'`
+   res=`echo "select count(*) from upload" | $src_path/runsql - | head -3 | grep '\s*0\s*'`
    if [ $? == 0 ]
    then
      echo "No uploads, alerting."
