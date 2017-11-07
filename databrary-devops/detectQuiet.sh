0a2c5b81b63eb76fef49be82e8d2a69daa279e82
Kanishka <nish2575@gmail.com>
2017-11-07 10:37:37 -0500

    Create detectQuiet.sh
    Script to detect there are no uploads in progress

---
+++ databrary-devops/detectQuiet.sh
@@ -0,0 +1,17 @@
+#!/bin/bash                                                                     
+# expects an argument providing the email address to alert
+
+while true
+do
+   res=`echo "select count(*) from upload" | ./runsql - | head -3 | grep '\s*0\\
+s*'`
+   if [ $? == 0 ]
+   then
+     echo "No uploads, alerting."
+     echo "Subject: Uploads have stopped" | sendmail -v $1                                   
+     exit 0
+   else
+     echo "Uploads in progress."
+   fi
+   sleep 5
+done
