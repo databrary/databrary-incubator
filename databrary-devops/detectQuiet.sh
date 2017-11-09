fa42bc4f6c97e9bebabd4747a638f49ba6d746cc
Kanishka <nish2575@gmail.com>
2017-11-09 14:48:13 -0500

    Don't use runsql, rely on generated pgpass
    might need localhost or other postgres config params to work

--- databrary-devops/detectQuiet.sh
+++ databrary-devops/detectQuiet.sh
@@ -2,9 +2,6 @@
 # expects one arguments:
 #   1. the email address to alert
 # ensure running as databrary (or demo, respectively) to connect to proper db
-# set these appropriately:
-src_path="/home/build/databrary"
-conf_dir="/home/databrary"
 
 cd $conf_dir
 while true
@@ -12,7 +9,7 @@ do
    # upload tokens expire a week after creation, so one that expire in more than 6 days from now are probably active
    # third line is the actual count
    # store result in res to avoid printing output
-   res=`echo "select count(*) from upload where expires > CURRENT_TIMESTAMP + interval '6 days'" | $src_path/runsql - | head -3 | grep '\s*0\s*'`
+   res=`echo "select count(*) from upload where expires > CURRENT_TIMESTAMP + interval '6 days'" | psql | head -3 | grep '\s*0\s*'`
    if [ $? == 0 ]
    then
      echo "No uploads, alerting."
