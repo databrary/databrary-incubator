5cd3c14835b3da1bdaa281307769b0a11c5dd9f2
Kanishka <nish2575@gmail.com>
2017-11-09 15:55:20 -0500

    Update detectQuiet.sh
    rollback psql change. need to figure out why it attempt to go to user's home directory as databrary user

--- databrary-devops/detectQuiet.sh
+++ databrary-devops/detectQuiet.sh
@@ -2,13 +2,14 @@
 # expects one arguments:
 #   1. the email address to alert
 # ensure running as databrary (or demo, respectively) to connect to proper db (use whoami)
+cd /home/databrary
 
 while true
 do
    # upload tokens expire a week after creation, so one that expire in more than 6 days from now are probably active
    # third line is the actual count
    # store result in res to avoid printing output
-   res=`echo "select count(*) from upload where expires > CURRENT_TIMESTAMP + interval '6 days'" | psql -U databrary databrary | head -3 | grep '\s*0\s*'`
+   res=`echo "select count(*) from upload where expires > CURRENT_TIMESTAMP + interval '6 days'" | /home/build/databrary/runsql - | head -3 | grep '\s*0\s*'`
    if [ $? == 0 ]
    then
      echo "No uploads, alerting."
