ebd03b8ecc0a72fd59645822608f26b5bd96a297
Kanishka <nish2575@gmail.com>
2017-11-22 18:32:08 -0500

    Update build-deploy-from-scratch.sh
    Make tmpdir explicit

--- databrary-devops/build-deploy-from-scratch.sh
+++ databrary-devops/build-deploy-from-scratch.sh
@@ -9,4 +9,4 @@ cd $BASEDIR
 git clone --branch $BRANCH --depth 1 https://github.com/databrary/databrary databrary-$BRANCH
 cd databrary-$BRANCH
 # trigger config file creation
-./build-package-copy-prep $TGT1
+TMPDIR=/tmp ./build-package-copy-prep $TGT1
