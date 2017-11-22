e681e2631e7a24e9c51383a326670dbad3d81b32
Kanishka <nish2575@gmail.com>
2017-11-22 18:43:06 -0500

    Create build-from-scratch.sh

---
+++ databrary-devops/build-from-scratch.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+BRANCH="master"
+BASEDIR=~/dev
+rm -rf $BASEDIR/databrary-$BRANCH
+mkdir -p $BASEDIR 
+cd $BASEDIR
+git clone --branch $BRANCH --depth 1 https://github.com/databrary/databrary databrary-$BRANCH
+cd databrary-$BRANCH
+TMPDIR=/tmp nix-build --attr pkgs.databrary --show-trace
