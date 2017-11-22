07cdaf4fccd4ff70ac96d7640f0a6c685ce4204b
Kanishka <nish2575@gmail.com>
2017-11-22 18:33:25 -0500

    Update build-deploy-from-scratch.sh

--- databrary-devops/build-deploy-from-scratch.sh
+++ databrary-devops/build-deploy-from-scratch.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 BRANCH="master"
+COPYUSR=`whoami`
 TGT1=$1
 BASEDIR=~/src
 rm -rf $BASEDIR/databrary-$BRANCH
@@ -9,4 +10,4 @@ cd $BASEDIR
 git clone --branch $BRANCH --depth 1 https://github.com/databrary/databrary databrary-$BRANCH
 cd databrary-$BRANCH
 # trigger config file creation
-TMPDIR=/tmp ./build-package-copy-prep $TGT1
+TMPDIR=/tmp ./build-package-copy-prep $COPYUSER@$TGT1
