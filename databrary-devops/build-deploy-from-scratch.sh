bcb520d5d697143e97d4825532940a1a34985e4b
Kanishka <nish2575@gmail.com>
2017-11-22 18:31:24 -0500

    Update build-deploy-from-scratch.sh
    make clone base dir a parameter

--- databrary-devops/build-deploy-from-scratch.sh
+++ databrary-devops/build-deploy-from-scratch.sh
@@ -2,10 +2,11 @@
 set -e
 BRANCH="master"
 TGT1=$1
-rm -rf ~/src/databrary-$BRANCH
-mkdir -p ~/src 
-cd ~/src
+BASEDIR=~/src
+rm -rf $BASEDIR/databrary-$BRANCH
+mkdir -p $BASEDIR 
+cd $BASEDIR
 git clone --branch $BRANCH --depth 1 https://github.com/databrary/databrary databrary-$BRANCH
-cd ~/src/databrary-$BRANCH
+cd databrary-$BRANCH
 # trigger config file creation
 ./build-package-copy-prep $TGT1
