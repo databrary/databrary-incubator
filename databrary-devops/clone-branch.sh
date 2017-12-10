9c67cb17363a8f8bc33755fc7c91a4ad26773721
Kanishka <nish2575@gmail.com>
2017-12-09 23:27:20 -0500

    Create clone-branch.sh

---
+++ databrary-devops/clone-branch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+branch=$1
+src_dir=/home/centos/src
+
+cd $src_dir
+
+echo "==== Delete prior cloned branch, database, and uploads"
+rm -rf $src_dir/databrary
+
+echo "==== Clone single branch $branch to $src_dir/databrary with history trimmed to last 5 commits"
+git clone http://github.com/databrary/databrary --branch $branch --depth 5
+
+echo "==== Restoring config/email from $src_dir"
+cd databrary
+cp $src_dir/config.email $src_dir/databrary/config/email
