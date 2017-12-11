fe204801de2ba90941488f404f8f4239b8fedb2c
kanishka <nish2575@gmail.com>
2017-12-11 18:27:57 -0500

    Wrap deploy with remote ssh scripts

---
+++ databrary-devops/ssh-deploy-stage.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+ssh -t $1@devdatabrary2.home.nyu.edu " \
+     cd ~ \
+  && rm -rf ~/databrary-incubator \
+  && git clone http://github.com/databrary/databrary-incubator \
+  && ~/databrary-incubator/databrary-devops/deploy-stage.sh develop \
+"
