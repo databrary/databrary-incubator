4e629bec9bff6ac5f68cd027dda0c902e0b78acf
kanishka <nish2575@gmail.com>
2017-12-11 14:27:39 -0500

    duplicate deploy-stage

---
+++ databrary-devops/deploy-prod.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+ENVIR="prod"
+BRANCH="master"
+COPYUSR=`whoami`
+BASEDIR=~/src
+clone_dir="$BASEDIR/databrary-$ENVIR-$BRANCH"
+rm -rf $BASEDIR/databrary-$ENVIR-*
+mkdir -p $BASEDIR 
+git clone --branch $BRANCH --depth 1 https://github.com/databrary/databrary $clone_dir
+cd $clone_dir
+# trigger config file creation
+TMPDIR=/tmp ./build-package-copy-prep-prod $COPYUSR
