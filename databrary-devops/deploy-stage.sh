a9e826c19ca3d52ea66d6f26c2bd83fccefc5e00
kanishka <nish2575@gmail.com>
2017-12-11 14:01:03 -0500

    Stage use develop branch

---
+++ databrary-devops/deploy-stage.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+ENVIR="stage"
+BRANCH="develop"
+COPYUSR=`whoami`
+BASEDIR=~/src
+clone_dir="$BASEDIR/databrary-$ENVIR-$BRANCH"
+rm -rf $clone_dir
+mkdir -p $BASEDIR 
+git clone --branch $BRANCH --depth 1 https://github.com/databrary/databrary $clone_dir
+cd $clone_dir
+# trigger config file creation
+TMPDIR=/tmp ./build-package-copy-prep-stage $COPYUSER
