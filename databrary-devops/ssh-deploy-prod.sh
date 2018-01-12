184a8c0c95963175869b4a12ad9a0cf6983834f7
Kanishka <nish2575@gmail.com>
2018-01-11 19:12:28 -0500

    Don't delete/clone databrary-incubator
    - Don't interfere with a staging deploy that might be running concurrently. This assuming a staging deploy will always have been run first.
    - Eventually change this to check for existence of databrary-incubator (crash if not) and pull

--- databrary-devops/ssh-deploy-prod.sh
+++ databrary-devops/ssh-deploy-prod.sh
@@ -2,7 +2,5 @@
 set -e
 ssh -t $1@devdatabrary2.home.nyu.edu " \
      cd ~ \
-  && rm -rf ~/databrary-incubator \
-  && git clone http://github.com/databrary/databrary-incubator \
   && ~/databrary-incubator/databrary-devops/deploy-prod.sh \
 "
