4528fb0b8557d3fc19f672ab57781c898e412e40
Kanishka <nish2575@gmail.com>
2018-01-24 08:45:31 -0500

    Create restore.sh

---
+++ databrary-devops/restore.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+for f in /nyu/dbdump/`date +%F`T04:00:* ; do
+        today=$f
+done
+dump=${1:-$today}
+if [[ ! -r $dump ]] ; then
+        echo "Current dump ($today) not found."
+        echo "Usage: $0 [DUMP]"
+        exit 1
+fi
+psql -d demo -h localhost -f reset.sql
+pg_restore -d demo -h localhost -O $dump
+rm -rf /nyu/demo/store/* /nyu/demo/upload/*
+rm -f hpc/databrary/demo/*
+ssh hpc rm -f databrary/demo/\*
