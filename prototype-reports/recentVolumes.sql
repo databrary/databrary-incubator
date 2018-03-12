167fe5c5ab89f4512fcc59fc01b4ff677d7ceab6
Kanishka <nish2575@gmail.com>
2018-03-12 13:09:43 -0400

    Update recentVolumes.sql

--- prototype-reports/recentVolumes.sql
+++ prototype-reports/recentVolumes.sql
@@ -1,4 +1,10 @@
-select * 
+select id, left(name, 50), alias, doi, audit_time, audit_user, 
+       (select count(*)
+       from volume_access va
+       where va.volume = av.id
+       and va.party = -1
+       and va.individual = 'PUBLIC') as public_can_access    
 from audit.volume av
 where av.audit_action = 'add'
-order by av.audit_time desc;
+order by av.audit_time desc
+limit 20;
