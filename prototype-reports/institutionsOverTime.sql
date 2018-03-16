942dc807ef56f6290b4c9eaae34d57151a6395eb
Kanishka <nish2575@gmail.com>
2018-03-16 16:32:54 -0400

    Update institutionsOverTime.sql

--- prototype-reports/institutionsOverTime.sql
+++ prototype-reports/institutionsOverTime.sql
@@ -1,6 +1,11 @@
-SELECT * FROM audit.party WHERE id in
-        (
-          SELECT child FROM authorize_view join party on authorize_view.child = party.id
-            WHERE parent = 0 AND child > 4 AND site = 'ADMIN'
-        )
-AND audit_action = 'add'
+SELECT monthp, sum(cnt) OVER (ORDER BY monthp) as cum_cnt
+FROM
+       (SELECT  date_trunc('month', a.audit_time) as monthp, count(*) as cnt
+        FROM 
+               (SELECT * FROM audit.party WHERE id in
+                        (
+                          SELECT child FROM authorize_view join party on authorize_view.child = party.id
+                            WHERE parent = 0 AND child > 4 AND site = 'ADMIN'
+                        )
+                AND audit_action = 'add') a 
+       ) b
