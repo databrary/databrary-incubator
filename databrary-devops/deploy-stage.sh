b47c578ea815bb3afce12e07eb8f49a32017024b
Kanishka <nish2575@gmail.com>
2018-03-20 10:59:10 -0400

    Update deploy-stage.sh

--- databrary-devops/deploy-stage.sh
+++ databrary-devops/deploy-stage.sh
@@ -4,7 +4,7 @@ ENVIR="stage"
 BRANCH=$1
 COPYUSR=`whoami`
 BASEDIR=~/src
-clone_dir="$BASEDIR/databrary-$ENVIR-$BRANCH"
+clone_dir="$BASEDIR/databrary-$ENVIR-$BRANCH"  # TODO: lowercase branch name to workaround gargoyle/network bug
 rm -rf $BASEDIR/databrary-$ENVIR-*
 mkdir -p $BASEDIR 
 git clone --branch $BRANCH --depth 1 https://github.com/databrary/databrary $clone_dir
