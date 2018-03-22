ea8162e352a10f62d2aacd9a987e7cd48dbd54f0
Kanishka <nish2575@gmail.com>
2018-03-22 18:06:42 -0400

    Create uniqueHighlightsBrowsed.sql

---
+++ prototype-reports/uniqueHighlightsBrowsed.sql
@@ -0,0 +1,5 @@
+-- trace through system for data logged when playing an excerpt and traversing to the excerpt
+
+-- get excerpts
+-- join with activity monitoring assets views/downloads (ignore whether action related to specific segment or not)
+-- extract distinct users
