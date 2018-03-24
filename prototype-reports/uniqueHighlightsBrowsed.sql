86dc13106a455516f8bc082689dfba62b4501e7c
Kanishka <nish2575@gmail.com>
2018-03-23 23:31:08 -0400

    Delete uniqueHighlightsBrowsed.sql

--- prototype-reports/uniqueHighlightsBrowsed.sql
+++
@@ -1,5 +0,0 @@
--- trace through system for data logged when playing an excerpt and traversing to the excerpt
-
--- get excerpts
--- join with activity monitoring assets views/downloads (ignore whether action related to specific segment or not)
--- extract distinct users
