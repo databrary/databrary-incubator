217d3e7872cbe2b0115594a589b5d5161ae06422
Kanishka <nish2575@gmail.com>
2018-04-02 10:41:30 -0400

    Update migrate-run-stage.sh

--- databrary-devops/migrate-run-stage.sh
+++ databrary-devops/migrate-run-stage.sh
@@ -6,9 +6,10 @@ cd /home/demo
 # run any new migrations, if any
 ./runNewDbMigrations.sh
 
-# clear out migrations script for next run
+# clear out migrations script, prepare for next run
 mv runNewDbMigrations.sh runNewDbMigrations.sh.last
 echo 'cd $(dirname $(readlink /home/demo/databraryExeLink))/../share/x86_64-linux-ghc-8.0.2/databrary-1/schema/' > /home/demo/runNewDbMigrations.sh
+chmod +x /home/demo/runNewDbMigrations.sh
 
 # print out timestamp of latest build
 stat -c '%y %N' databraryExeLink
