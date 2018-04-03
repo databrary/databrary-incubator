47604e6ec1bde60ee1d815d4e8aee347db2967d0
Kanishka <nish2575@gmail.com>
2018-04-03 18:16:44 -0400

    Update ingestCopyFiles.sql

--- prototype-reports/ingestCopyFiles.sql
+++ prototype-reports/ingestCopyFiles.sql
@@ -4,3 +4,9 @@ select
 from slot_asset sa 
   inner join asset a on sa.asset = a.id 
 where container = 25581;
+
+-- copy into shell script
+-- add mkdir at top of script
+-- bash scriptname.sh  - can take up to ten minutes to complete, watch top
+
+-- generate manifest of copied files by using same query with only target ouptut
