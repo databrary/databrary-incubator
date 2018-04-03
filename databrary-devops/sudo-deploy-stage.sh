11f280bf92b1a8459b7886dd0216bd70436e175c
Kanishka <nish2575@gmail.com>
2018-04-02 20:34:26 -0400

    Delete sudo-deploy-stage.sh

--- databrary-devops/sudo-deploy-stage.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-set -e
-sudo su - ka988 -c " cd ~ && rm -rf ~/databrary-incubator && git clone http://github.com/databrary/databrary-incubator && ~/databrary-incubator/databrary-devops/deploy-stage.sh $1 "
