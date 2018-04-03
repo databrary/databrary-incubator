2ea77e4d1a3ff94f135490fc10e0a3125962b52f
Kanishka <nish2575@gmail.com>
2018-04-02 20:35:30 -0400

    Delete ssh-deploy-stage-2.sh

--- databrary-devops/ssh-deploy-stage-2.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# should take optional branch name as a parameter, and default to develop when nothing provided
-set -e
-ssh -t $1@devdatabrary2.home.nyu.edu " \
-  sudo su - ka988 -c " cd ~ && rm -rf ~/databrary-incubator && git clone http://github.com/databrary/databrary-incubator && ~/databrary-incubator/databrary-devops/deploy-stage.sh $2 " \
-"
