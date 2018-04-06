0f629f212f213e7f7b5eb367e8d9417583a32ff2
Kanishka <nish2575@gmail.com>
2018-04-06 10:41:37 -0400

    Update recentVolumes.sql

--- prototype-reports/recentVolumes.sql
+++ prototype-reports/recentVolumes.sql
@@ -1,11 +1,11 @@
 -- wanted: public vs partial vs private 
 
-select id, left(name, 50), alias, doi, audit_time, audit_user, 
+select id, left(name, 50), alias, doi, audit_time, audit_user as usr, 
        (select count(*)
        from volume_access va
        where va.volume = av.id
        and va.party = -1
-       and va.individual = 'PUBLIC') as public_can_access    
+       and va.individual = 'PUBLIC') as public
 from audit.volume av
 where av.audit_action = 'add'
 order by av.audit_time desc
