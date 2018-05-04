9170506adabb4968aada0f17f40e0eaa824c7ca7
Kanishka <kanishka.azimi@databrary.org>
2018-05-04 16:25:25 -0400

    Update recentVolumes.sql

--- prototype-reports/recentVolumes.sql
+++ prototype-reports/recentVolumes.sql
@@ -1,11 +1,11 @@
--- wanted: public vs partial vs private 
-
 select id, left(name, 50), alias, doi, audit_time, audit_user as usr, 
-       (select count(*)
+       (select case va.share_full when true then 'PUBLIC'
+                                  when false then 'PARTIAL' end
        from volume_access va
        where va.volume = av.id
        and va.party = -1
-       and va.individual = 'PUBLIC') as public
+       and va.individual = 'PUBLIC'
+       limit 1) as share_status
 from audit.volume av
 where av.audit_action = 'add'
 order by av.audit_time desc
