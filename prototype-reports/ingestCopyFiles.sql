c61a98c9cc058615ee17b83452efbc0bb3ef7c58
Kanishka <kanishka.azimi@databrary.org>
2018-05-11 09:56:28 -0400

    Update ingestCopyFiles.sql

--- prototype-reports/ingestCopyFiles.sql
+++ prototype-reports/ingestCopyFiles.sql
@@ -4,7 +4,7 @@ select
   || ' /nyu/stage/ebergelson/' || volume || '/' || container || '/' || name 
 from slot_asset sa 
   inner join asset a on sa.asset = a.id 
-where container = 25581;
+where container = <SLOT ID>;
 
 -- original
 select 
@@ -14,10 +14,14 @@ from slot_asset sa
   inner join asset a on sa.asset = a.id  
   inner join transcode t on t.asset = a.id
   inner join asset oa on t.orig = oa.id
-where sa.container = 25581;
+where sa.container = <SLOT ID>;
+
 
 -- copy into shell script
 -- add mkdir at top of script
+-- add set -x to echo command, as they take time to run
 -- bash scriptname.sh  - can take up to ten minutes to complete, watch top
+
+-- run the converted copy before the original copy, so that the converted "wins" in any conflict
 
 -- generate manifest of copied files by using same query with only target ouptut (or ls -1 in the output dir)
