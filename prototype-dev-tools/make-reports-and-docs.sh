3da0825f2cab3a87f715299422b876e638e39b21
Bryan Richter <b@chreekat.net>
2018-05-14 11:05:40 -0400

    Auto push gh-pages; Print summary

--- prototype-dev-tools/make-reports-and-docs.sh
+++ prototype-dev-tools/make-reports-and-docs.sh
@@ -4,12 +4,24 @@
 # Use
 # https://discourse.nixos.org/t/best-way-to-augment-a-nix-shell-for-dev-utilities/157/6
 # once we finally update nixpkgs again.
-set -eux
+set -eu
 
+# TODO: Include commit date as well as report creation date.
+# TODO: Clean up hpc summary
 TODAY=$(date +%Y-%m-%d)
 
 DISTDIR=dist-reports
 CABAL="cabal --builddir=$PWD/$DISTDIR"
+
+haddock_report_file=haddock-coverage-report-${TODAY}.txt
+
+make_haddock_summary () {
+    perl -ne '
+        BEGIN { $n = 0; $d = 0; }
+        m%(\d+) */ *(\d+)% or next;
+        $n += $1; $d += $2;
+        END { printf "%.2f%% %d %d\n",  ($n / $d) * 100,  $n, $d; }' $@
+}
 
 build () {
     ## Basic (?) cabal steps
@@ -24,7 +36,7 @@ build () {
 
     ## Use cabal to call haddock
     $CABAL haddock --hyperlink-source | grep ') in' \
-        | tee haddock-coverage-report-${TODAY}.txt
+        | tee $haddock_report_file
 }
 
 report () {
@@ -45,8 +57,25 @@ report () {
         cd ${wd}
         trap "cd -" EXIT
         git commit --no-gpg-sign --no-verify --all -m "Update ${TODAY}"
+        git push
     )
+}
+
+summary () {
+    echo
+    echo '##################################################'
+    echo '#####                SUMMARY                 #####'
+    echo '##################################################'
+    echo 'Haddock coverage numbers:'
+    echo -n '    '
+    make_haddock_summary $haddock_report_file
+    echo 'HPC coverage numbers:'
+    echo -n '    '
+    tail -2 $DISTDIR/hpc/vanilla/html/databrary-1/hpc_index.html \
+        | head -1 \
+        | grep -o -P '^.{40}'
 }
 
 build
 report
+summary
