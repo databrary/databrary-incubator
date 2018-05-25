845c1f9b3b764ff0b127de610e1706c05e245ff1
Kanishka <kanishka.azimi@databrary.org>
2018-05-25 14:16:33 -0400

    Update uniqueVolumeZipDownloads.sql

--- prototype-reports/uniqueVolumeZipDownloads.sql
+++ prototype-reports/uniqueVolumeZipDownloads.sql
@@ -6,4 +6,13 @@ select
       ) as usrcnt
 from audit.volume 
 where audit_action = 'open' 
-group by id order by usrcnt desc
+group by id order by usrcnt desc;
+
+-- rollup over user instead of volume; use all downloads of volume, not distinct
+select 
+     case when audit_user = -1 then 'IP: ' || cast(audit_ip as varchar(20)) else cast(audit_user as varchar(10)) end as usr
+   , count(*) 
+from audit.volume 
+where audit_action = 'open' 
+group by usr 
+order by usr asc;
