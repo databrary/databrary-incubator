3238fe8922c02958baaa34dfade8d7db9266cfb3
Kanishka <kanishka.azimi@databrary.org>
2018-05-25 13:45:15 -0400

    Update recentVolumes.sql

--- prototype-reports/recentVolumes.sql
+++ prototype-reports/recentVolumes.sql
@@ -1,6 +1,6 @@
-select id
-       , left(name, 50) , alias, doi
-       , to_char(audit_time, 'Mon DD YYYY HH12:MI AM') as audit_time, audit_user as usr, 
+select av.id
+       , p.prename, p.name, doi
+       , to_char(audit_time, 'Dy Mon DD') as audit_time, audit_user as usr, 
        (select case va.share_full when true then 'PUBLIC'
                                   when false then 'PARTIAL' end
        from volume_access va
@@ -9,6 +9,7 @@ select id
        and va.individual = 'PUBLIC'
        limit 1) as share_status
 from audit.volume av
+  inner join party p on av.audit_user = p.id
 where av.audit_action = 'add'
 order by av.audit_time desc
 limit 20;
