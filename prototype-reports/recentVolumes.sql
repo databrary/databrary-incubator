497f6e30795baf9a9cdf81a0d43cda7d4b02c62f
Kanishka <kanishka.azimi@databrary.org>
2018-05-25 22:13:57 -0400

    Update recentVolumes.sql

--- prototype-reports/recentVolumes.sql
+++ prototype-reports/recentVolumes.sql
@@ -1,15 +1,17 @@
 select av.id
-       , p.prename, p.name, doi
-       , to_char(audit_time, 'Dy Mon DD') as audit_time, audit_user as usr, 
-       (select case va.share_full when true then 'PUBLIC'
-                                  when false then 'PARTIAL' end
-       from volume_access va
-       where va.volume = av.id
-       and va.party = -1
-       and va.individual = 'PUBLIC'
-       limit 1) as share_status
+       -- , p.prename, p.name (only show for admin)
+       , doi
+       , to_char(audit_time, 'Dy Mon DD') as audit_time
+       -- , audit_user as usr (only show for admin) 
+       , (select case va.share_full when true then 'PUBLIC'
+                                    when false then 'PARTIAL' end
+          from volume_access va
+          where va.volume = av.id
+            and va.party = -1
+            and va.individual = 'PUBLIC'
+            limit 1) as share_status
 from audit.volume av
   inner join party p on av.audit_user = p.id
 where av.audit_action = 'add'
 order by av.audit_time desc
-limit 20;
+limit 20;  -- make limit configurable up to a maximum
