2a3f65664a573ac882d7ba87b4a2c16d25336d38
Kanishka <kanishka.azimi@databrary.org>
2018-05-31 16:03:16 -0400

    Add format extension, coalesce for unconverted files

--- prototype-reports/ingestCopyFiles.sql
+++ prototype-reports/ingestCopyFiles.sql
@@ -1,20 +1,27 @@
--- converted
+-- converted and unconverted
+---- this will retrieve all converted assets combined with all assets that didn't need any conversion
 select 
-  'cp /nyu/store/' || substr(cast(sha1 as varchar(80)), 3, 2) || '/' || right(cast(sha1 as varchar(80)), -4) 
-  || ' /nyu/stage/ebergelson/' || volume || '/' || container || '/' || name 
-     || '.' || (select extension[1] from format where id = a.format) 
+  'cp /nyu/store/' || substr(cast(sha1 as varchar(80)), 3, 2) 
+     || '/' || right(cast(sha1 as varchar(80)), -4) 
+  || ' /nyu/stage/ebergelson/' || volume || '/' || container || '/' 
+     || a.name || '.' || f.extension[1] 
 from slot_asset sa 
   inner join asset a on sa.asset = a.id 
+  inner join format f on a.format = f.id
 where container = <SLOT ID>;
 
--- original
+-- original and unconverted
+---- this will retrieve all original assets combined with all assets that didn't need any conversion 
 select 
-  'cp /nyu/store/' || substr(cast(oa.sha1 as varchar(80)), 3, 2) || '/' || right(cast(oa.sha1 as varchar(80)), -4) 
-  || ' /nyu/stage/ebergelson/' || a.volume || '/' || sa.container || '/ORIG-' || oa.name
+  'cp /nyu/store/' || substr(cast(coalesce(oa.sha1, a.sha1) as varchar(80)), 3, 2) 
+     || '/' || right(cast(coalesce(oa.sha1, a.sha1) as varchar(80)), -4) 
+  || ' /nyu/stage/ebergelson/' || a.volume || '/' || sa.container || '/' 
+     || coalesce(oa.name, a.name || '.' || f.extension[1])
 from slot_asset sa 
   inner join asset a on sa.asset = a.id  
-  inner join transcode t on t.asset = a.id
-  inner join asset oa on t.orig = oa.id
+  inner join format f on a.format = f.id
+  left join transcode t on t.asset = a.id
+  left join asset oa on t.orig = oa.id
 where sa.container = <SLOT ID>;
 
 
@@ -24,10 +31,8 @@ where sa.container = <SLOT ID>;
 -- first line: set -x (to echo command, as they take time to run)
 -- next line: mkdir -p [path] at top of script ... DO NOT FORGET /NYU/STAGE!!!!!!
 -- bash scriptname.sh  - can take up to ten minutes to complete, watch top
--- add , a.format after || name to see extensions
 
--- run the converted copy before the original copy, so that the converted "wins" in any conflict
--- add , oa.format after || oa.name to see extensions
+-- run only converted or original at any given time, not both. okay to run the other at a later time.
 
 -- generate manifest of copied files by using same query with only target ouptut (or ls -1 in the output dir)
 
