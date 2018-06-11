6e498a59592979d302dc81c38bcf8431a0d1d945
Kanishka <kanishka.azimi@databrary.org>
2018-06-11 10:53:12 -0400

    Update outageNotifications.hs

--- databrary-backend-design-reviews/outageNotifications.hs
+++ databrary-backend-design-reviews/outageNotifications.hs
@@ -1,4 +1,15 @@
-type UserNoticeMessage = String
+{-
+existing type:
+module D.S.Types
+
+data Service = Service
+  { serviceStartTime :: !Timestamp
+  ...
+  , serviceDown :: !(Maybe T.Text)
+  }
+-}
+
+type UserNoticeMessage = String -- notification message will be propagated to the front end for display.
 
 -- | Mutually exclusive service levels
 data ServiceLevel = 
@@ -6,16 +17,69 @@ data ServiceLevel =
    | DegradedTranscodingDown UserNoticeMessage
    | FullyOperational
   deriving (Ord)
- 
- -- | 
- data TranscodingServiceLevel = TranscodingDown | FullyOperational
+
+data Service = Service
+  { serviceStartTime :: !Timestamp
+  ...
+  , serviceLevel :: !ServiceLevel
+  }
+
+{- existing initialization logic:
+module D.S.Init
+
+initService
+    :: Bool -- ^ Run in foreground?
+    -> C.Config
+    -> IO Service
+initService fg conf = do
+  time <- getCurrentTime
+  ...
+  storage <- initStorage (conf C.! "store")
+  let rc = Service
+        { serviceStartTime = time
+        ...
+        , serviceStorage = storage
+        ...
+        , serviceDown = conf C.! "store.DOWN"
+        }
+  return $! rc
+    { servicePeriodic = periodic
+    }
+
+module D.Store.Service
+
+initStorage :: C.Config -> IO Storage
+initStorage conf
+  | Just down <- conf C.! "DOWN" = return $ error $ "Storage unavailable: " ++ down
+  | otherwise = do
+      ...
+      
+      tc <- initTranscoder (conf C.! "transcode")
+
+      return $ Storage
+        { storageMaster = master
+        ...
+        , storageTranscoder = tc
+        }
+        
+module D.Store.Transcoder
+
+initTranscoder :: C.Config -> IO (Maybe Transcoder)
+initTranscoder conf =
+  ...
+  Just <$> do
+      ...
+      (r, out, err) <- runTranscoder t ["-t"]
+      case r of
+        ExitSuccess -> return t
+        ExitFailure e -> fail $ "initTranscoder test: " ++ show e ++ "\n" ++ out ++ err
+-}
+
+data TranscodingServiceLevel = TranscodingDown | FullyOperational
    deriving (Ord)
- 
- data StorageServiceLevel = StorageDown | FullyOperational
-   deriving (Ord)
- 
- -- utiity used during transcoding init
- 
- 
- -- utility used during storage init
- 
+
+initStorage :: C.Config -> ServiceLevel -> IO Storage
+
+-- initStorage will now branch on ServiceLevel.
+
+initTranscoder :: C.Config -> TranscodingServiceLevel -> IO (Maybe Transcoder)
