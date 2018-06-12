1e4699979598d15fccf9072bdf527b4a7a952ff5
Kanishka <kanishka.azimi@databrary.org>
2018-06-12 09:54:58 -0400

    Update clusterOutageJobs.sql

--- prototype-reports/clusterOutageJobs.sql
+++ prototype-reports/clusterOutageJobs.sql
@@ -1,6 +1,8 @@
--- update start date as desired
+-- when process is null and (asset is missing size or something?) and log message is "prince is down", 
+--  then those are the failures
+
 select orig, asset, owner, segment, start, process, substring(log from 0 for 50) 
 from transcode 
-where process is not null 
-and start > '2018-06-10' 
+where start > '2018-06-10'  -- update start date as desired
 order by start desc;
+
