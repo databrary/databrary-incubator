151feb3bf4ead9b8ffdf15c60eea1af3fb1aee21
Kanishka <kanishka.azimi@databrary.org>
2018-07-25 19:33:06 -0400

    Update deploy-prod.sh

--- databrary-devops/deploy-prod.sh
+++ databrary-devops/deploy-prod.sh
@@ -2,7 +2,6 @@
 set -e
 ENVIR="prod"
 BRANCH="master"
-COPYUSR=`whoami`
 BASEDIR=~/src
 clone_dir="$BASEDIR/databrary-$ENVIR-$BRANCH"
 rm -rf $BASEDIR/databrary-$ENVIR-*
@@ -11,4 +10,4 @@ git clone --branch $BRANCH --depth 1 https://github.com/databrary/databrary $clo
 cd $clone_dir
 git log -1
 # trigger config file creation
-TMPDIR=/tmp ./build-package-copy-prep-prod $COPYUSR
+TMPDIR=/tmp ./build-package-copy-prep-prod
