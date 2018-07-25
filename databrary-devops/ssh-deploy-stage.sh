db2cdfd4c53368ed7aab651eec0339754d063242
Kanishka <kanishka.azimi@databrary.org>
2018-07-25 19:31:47 -0400

    Update ssh-deploy-stage.sh

--- databrary-devops/ssh-deploy-stage.sh
+++ databrary-devops/ssh-deploy-stage.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # should take optional branch name as a parameter, and default to develop when nothing provided
 set -e
-ssh -t $1@devdatabrary2.home.nyu.edu " \
+ssh -t datadeploy@devdatabrary2.home.nyu.edu " \
      cd ~ \
   && rm -rf ~/databrary-incubator \
   && git clone http://github.com/databrary/databrary-incubator \
