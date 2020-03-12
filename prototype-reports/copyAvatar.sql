39e3d2d30fe8830eb5a6e0a1e6effc7de8cc9555
Reda Nezzar <nezzar.reda@gmail.com>
2020-03-12 12:00:07 -0400

    Update copyAvatar.sql

--- prototype-reports/copyAvatar.sql
+++ prototype-reports/copyAvatar.sql
@@ -1,7 +1,6 @@
 COPY (SELECT 
     'cp /nyu/store/' || substr(cast(a.sha1 as varchar(80)), 3, 2) 
      || '/' || right(cast(a.sha1 as varchar(80)), -4) 
-  || ' /nyu/stage/reda/avatar/' || a.name || '.' || f.extension[1] 
+  || ' "/nyu/stage/reda/avatar/' || a.name || '"' 
 FROM asset a
-    INNER JOIN avatar av on av.asset = a.id
-    INNER JOIN format f on a.format = f.id) TO '/tmp/avatar.sh';
+    INNER JOIN avatar av on av.asset = a.id) TO '/tmp/avatar.sh';
